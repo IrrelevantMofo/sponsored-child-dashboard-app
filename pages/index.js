0c6cef07d238406923cee83c4879220ce3f736c7
joshua Irving B. Fabricante <joshua.starkiller115@gmail.com>
2021-09-10 02:24:00 +0800

    Update index.js
    Adds lifecycles for the index page of the app

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,34 @@
+import { useState, useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { fetchChildAsync } from '../store/sagas/children';
+
+
 
 export default function Home() {
+
+  const [loading, setLoading] = useState(false)
+  const { errorMessage, requestStatus } = useSelector(state => state.siteData);
+  const dispatch = useDispatch();
+  
+  useEffect(() => { 
+    return () => {
+      setLoading(false)
+    }
+  },[])
+
+  useEffect(()=>{
+    switch(requestStatus){
+      case 'NONE':
+        setLoading(false);
+      case 'REQUESTING':
+        setLoading(true);
+      case 'SUCCESS':
+        setLoading(false);
+      case 'FAILURE':
+        setLoading(false)
+    }
+  },[requestStatus])
+
   return (
     <div className='container'>
       <div className='row mt-3'>

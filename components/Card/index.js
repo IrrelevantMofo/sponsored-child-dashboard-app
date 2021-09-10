cecccdffaa3dd4c4ca4e99f68d07277a556d9a94
joshua Irving B. Fabricante <joshua.starkiller115@gmail.com>
2021-09-10 19:57:25 +0800

    Update index.js

--- components/Card/index.js
+++ components/Card/index.js
@@ -3,21 +3,28 @@ import { useEffect, useState } from "react";
 
 // core components
 function Card({data}) {
+    const [imgSrc, setImgSrc] = useState(data.imageUrl);
+
   return (<>
-            <div className="custom-card mr-1 ml-1 mt-1 pt-2 pb-2">
+            <div className="custom-card text-gray mr-1 ml-1 mt-1 pt-2 pb-2">
                 <div className="picture">
                     <img 
                         className="img-fluid"
-                        onError={e => { e.currentTarget.src = 'https://thesocietypages.org/socimages/files/2009/05/vimeo.jpg'; }}
-                        src={data.imageUrl} 
+                        width='130px'
+                        height='auto'
+                        onError={()=>{
+                            setImgSrc('https://thesocietypages.org/socimages/files/2009/05/vimeo.jpg')
+                        }}
+                        src={imgSrc.toString()} 
                         alt="Avatar"
+                        layout='fill'
                     />
                 </div>
-                <div className="team-content">
-                    <h3 className="name">{data.firstName} {data.lastName} | {data.gender.toLowerCase()==='male'?<i className="fas fa-mars fa-lg" style={{color:'cyan', }}></i>:<i className="fas fa-venus fa-lg" style={{color:'pink'}}></i>}</h3>
-                    <span>
-                        {data.country}
-                    </span>
+                <div className="team-content mt-1">
+                    <h3 className="name text-secondary">{data.firstName} {data.lastName} | {data.gender.toLowerCase()==='male'?<i className="fas fa-mars fa-lg text-primary" ></i>:<i className="fas fa-venus fa-lg text-pink"></i>}</h3>
+                        <span>
+                            {data.country}
+                        </span>
                     <br/>
                     <br/>
                     <small style={{fontStyle: 'italic'}}>

167eae71645ed118ba5cb781545fc4c3b0dbd56f
joshua Irving B. Fabricante <joshua.starkiller115@gmail.com>
2021-09-10 17:47:55 +0800

    Update _app.js
    Adds cdn for font-awesome

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,20 @@
 import '../assets/styles/scss/index.scss'
 import { Provider } from "react-redux";
 import store from '../store/store';
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <>
+          <Head>
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1, shrink-to-fit=no"
+            />
+            <title>Sponsored Child Dashboard</title>
+            <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossOrigin="anonymous"/>
+          </Head>
         <Component {...pageProps} />
       </>
     </Provider>
